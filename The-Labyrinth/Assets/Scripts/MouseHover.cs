3aa25a9387cb456c0d8f34a797317c8d29da705a
Dylan Houston <dylanhouston7@gmail.com>
2016-09-20 17:44:18 -0400

    Added Main Menu (very rough)
    Dylan's first stab at a main menu

---
+++ The-Labyrinth/Assets/Scripts/MouseHover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseHover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    void OnMouseClick()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+}
