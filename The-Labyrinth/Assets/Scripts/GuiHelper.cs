c62fe44063ea3172eaa5e7efc2ea8cae12da358b
Dylan Houston <dylanhouston7@gmail.com>
2016-10-03 19:33:38 -0400

    Basic 30 second timer for the first maze
    Still need to add interactions with GameManager. This is merely to show
    professor at demo on 10/4/16

---
+++ The-Labyrinth/Assets/Scripts/GuiHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    /// <summary>
+    /// A class to assist with creating GUI elements
+    /// </summary>
+    public class GuiHelper
+    {
+        /// <summary>
+        /// Creates a GUIStyle object that can be used for labels
+        /// </summary>
+        /// <param name="fontSize">The size of the font</param>
+        /// <param name="font">The font style</param>
+        /// <returns>A GUIStyle that encapsulates font information</returns>
+        public static GUIStyle CreateGuiStyle(int fontSize, Font font)
+        {
+            // TODO: Add more options in this method.
+            GUIStyle guiStyle = new GUIStyle(); //create a new variable
+            guiStyle.fontSize = fontSize;
+            guiStyle.font = font;
+
+            // This doesn't work for some reason.
+            //guiStyle.alignment = TextAnchor.UpperCenter;
+
+            return guiStyle;
+        }
+    }
+}
