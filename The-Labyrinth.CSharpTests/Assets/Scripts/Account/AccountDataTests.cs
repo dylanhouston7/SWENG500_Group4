f357cc62e5a96852589734cdb492077c2c6534d4
responsivedevelopment <douglasjohngraham@gmail.com>
2016-11-27 19:15:58 -0500

    Create Testing Framework using Intellitest
    Create Testing Framework using Intellitest

---
+++ The-Labyrinth.CSharpTests/Assets/Scripts/Account/AccountDataTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Labyrinth.CSharpTests.Assets.Scripts.Account
+{
+    class AccountDataTests
+    {
+    }
+}
